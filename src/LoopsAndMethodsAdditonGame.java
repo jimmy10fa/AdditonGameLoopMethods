7736f3d2e53886bc601fb3cea33299fae4436ba7
Jeremy Evert <jeremy.evert@swosu.edu>
2015-11-11 09:20:41 -0600

    Back to the end of Monday.

--- src/LoopsAndMethodsAdditonGame.java
+++ src/LoopsAndMethodsAdditonGame.java
@@ -8,12 +8,20 @@ public class LoopsAndMethodsAdditonGame {
 	public static void AdditonGameMethod() {
 		System.out.println("Inside the addition game method.");
 		
+		int hardness = 5;
+		
 		// Set up my for loop to go through the number of rounds
 		int numberOfRounds = 2;
 		for(int roundNumber = 1; 
 		roundNumber <= numberOfRounds;  
 		roundNumber = roundNumber + 1){
 			System.out.println("Inside the for loop. Round: " + roundNumber);
+			boolean isAnswerCorrect = getAndCheckStudentAnswer(hardness);
 		}
+	}
+	
+	public static boolean getAndCheckStudentAnswer(int hardness) {
+		System.out.println("Inside get and check student answer method.");
+		return false;
 	}
 }
